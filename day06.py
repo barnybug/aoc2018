dcde8523780dbeaf0ce173e62cab0c785e54cca6
Barnaby Gray <barnaby@pickle.me.uk>
2018-12-06 22:16:08 +0000

    np day 6

--- day06.py
+++ day06.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import numpy as np
 from collections import Counter
 import re
 import string
@@ -10,26 +11,18 @@ def symbol(i):
     return symbols[i+1]
 
 def part1(filename):
-    coords = [tuple(map(int, re.findall(r'\d+', line))) for line in open(filename)]
+    coords = np.loadtxt(filename, delimiter=', ').astype(int)
+    # coords = [tuple(map(int, re.findall(r'\d+', line))) for line in open(filename)]
     mx = max(x for x, _ in coords)+1
     my = max(y for _, y in coords)+1
-    grid = [[-1 for x in range(mx)] for y in range(my)]
+    grid = np.full((my, mx), -1)
 
     for x in range(mx):
         for y in range(my):
-            nd = 10000
-            nc = None
-
-            for ci, (cx, cy) in enumerate(coords):
-                d = abs(cx-x) + abs(cy-y)
-                if d < nd:
-                    nd = d
-                    nc = ci
-                elif d == nd:
-                    nc = None
-
-            if nc is not None:
-                grid[y][x] = nc
+            ds = np.absolute(coords - (x, y)).sum(1)
+            m = ds.min()
+            if (ds == m).sum() == 1:
+                grid[y][x] = np.argmin(ds)
 
     # for row in grid:
     #     print(''.join(symbol(r) for r in row))
@@ -61,5 +54,5 @@ def part2(filename, below):
     return sum(1 for x in range(mx) for y in range(my) if grid[y][x] < below)
 
 if __name__ == '__main__':
-    # print(part1('input06.txt'))
+    print(part1('input06.txt'))
     print(part2('input06.txt', 10000))
