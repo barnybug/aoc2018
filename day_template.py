a9ee25198e0ac6d261977a21606daf853a2cec6c
Barnaby Gray <barnaby@pickle.me.uk>
2018-12-08 12:46:18 +0000

    Template

---
+++ day_template.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+def part1(filename):
+    pass
+
+def part2(filename):
+    pass
+
+if __name__ == '__main__':
+    print(part1('input_.txt'))
+    # print(part2('input_.txt'))
