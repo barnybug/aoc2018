63636a8bfe10bab9c1089a76688317282892dd10
Barnaby Gray <barnaby@pickle.me.uk>
2018-12-11 22:20:07 +0000

    Super fast numpy day 11

--- day11.py
+++ day11.py
@@ -10,36 +10,30 @@ def power_grid(serial):
     gr = power // 100 % 10 - 5
     return gr
 
-def max_power(gr, size):
-    max_coord = None
-    max_total = -10
-    for x in range(300-size):
-        for y in range(300-size):
-            total = gr[x:x+size, y:y+size].sum()
-            if total > max_total:
-                max_total = total
-                max_coord = (x+1, y+1)
-
-    return max_coord, max_total
-
 def part1(serial):
     gr = power_grid(serial)
-    return max_power(gr, 3)[0]
+    cs = gr.cumsum(0).cumsum(1)
+    size = 3
+    totals = cs[size:,size:] + cs[:-size,:-size] - cs[size:,:-size] - cs[:-size,size:]
+    x, y = np.unravel_index(
+        totals.argmax(), totals.shape)
+    return (x+2, y+2)
 
 def part2(serial):
     gr = power_grid(serial)
-    max_size = None
-    max_coord = None
-    max_total = -10
-    for size in range(1, 300):
-        print(size)
-        coord, total = max_power(gr, size)
-        if total > max_total:
-            max_coord = coord
-            max_total = total
-            max_size = size
+    cs = gr.cumsum(0).cumsum(1)
 
-    return max_coord[0], max_coord[1], max_size
+    answer = None
+    max_total = 0
+    for size in range(1, 300):
+        totals = cs[size:,size:] + cs[:-size,:-size] - cs[size:,:-size] - cs[:-size,size:]
+        if totals.max() > max_total:
+            x, y = np.unravel_index(
+                totals.argmax(), totals.shape)
+            answer = (x+2, y+2, size)
+            max_total = totals.max()
+
+    return answer
 
 if __name__ == '__main__':
     print(part1(1308))
